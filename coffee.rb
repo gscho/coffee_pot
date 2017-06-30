6f9d7d3e5a5b1c91004fd5ffb3c0017972e6048b
Greg Schofield <gregs@indellient.com>
2017-06-29 22:15:59 -0400

    Added db migration and signup page

--- coffee.rb
+++ coffee.rb
@@ -1,5 +1,26 @@
 require 'sinatra'
+require 'sinatra/activerecord'
+require './config/environments' #database configuration
+require './models/user'        #User class
 
 get '/' do
   erb :index
+end
+
+get '/signup' do
+  erb :signup
+end
+
+post '/new/user' do
+  @user = User.new(params[:user])
+  if @user.save
+    redirect '/success'
+  else
+    "Sorry, there was an error!"
+  end
+end
+
+get '/success' do
+	@users = User.all
+	erb :success
 end
