319fa45cf9830b27de95b3591f3a1af424f4a6cb
Greg Schofield <gregs@indellient.com>
2017-07-10 10:17:18 -0400

    Bind to host 0.0.0.0

--- config/environments.rb
+++ config/environments.rb
@@ -1,6 +1,7 @@
 #The environment variable DATABASE_URL should be in the following format:
 # => postgres://{user}:{password}@{host}:{port}/path
 configure :production, :development do
+
     db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/coffee')
 
     ActiveRecord::Base.establish_connection(
@@ -11,4 +12,6 @@ configure :production, :development do
             :database => db.path[1..-1],
             :encoding => 'utf8'
     )
+
+    set :bind, '0.0.0.0'
 end
