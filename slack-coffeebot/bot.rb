04f5dcdc0fd33c369b554cea3414533c019e9490
Greg Schofield <gregs@indellient.com>
2017-08-07 14:46:05 -0400

    Updated git ignore

--- slack-coffeebot/bot.rb
+++
@@ -1,4 +0,0 @@
-module SlackCoffeeBot
-  class Bot < SlackRubyBot::Bot
-  end
-end
--- slack-coffeebot/commands/getStatus.rb
+++
@@ -1,9 +0,0 @@
-module SlackCoffeeBot
-  module Commands
-    class GetStatus < SlackRubyBot::Commands::Base
-      command 'hello' do |client, data, _match|
-        client.say(channel: data.channel, text: 'Hello! I\'m the coffee bot!')
-      end
-    end
-  end
-end
